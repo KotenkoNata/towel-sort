45f389bc2652570c3f27d082949ea37bafad9992
KotenkoNata <kotenkonata8@gmail.com>
2022-01-25 10:44:55 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// module.exports =
-    function towelSort (matrix) {
+module.exports = function towelSort (matrix) {
         let result = [];
 
         if(matrix.length === 0){
@@ -9,27 +8,24 @@
         }
 
     for (let i = 0; i <= matrix.length-1; i++){
-        if( i % 2 === 0){
-            result = result.concat(matrix[i]);
-        }else{
-            for (let k=matrix[i].length-1; k >= 0; k--){
-                result.push(matrix[i][k])
-            }
+        for(let j = 0; j< matrix[i].length;j++) {
+            let column = i % 2 === 0
+            ? j
+            : (matrix[i].length - j - 1);
+
+            result.push(matrix[i][column]);
         }
 
-
-
-
-    }
+        }
   return result;
 }
 
 
-console.log(towelSort([
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-    ]));
+// console.log(towelSort([
+//         [1, 2, 3, 4],
+//         [5, 6, 7, 8],
+//         [9, 10, 11, 12],
+//         [13, 14, 15, 16],
+//     ]));
 
 //[ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]
