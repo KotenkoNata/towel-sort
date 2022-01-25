70929f28768eda78fc8226f6a70788beb9308f2c
KotenkoNata <kotenkonata8@gmail.com>
2022-01-25 10:54:23 +0200

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 module.exports = function towelSort (matrix) {
         let result = [];
 
-        if(matrix.length === 0){
-            return []
-        }else if(!matrix){
+        if(!matrix){
             return []
         }
 
@@ -26,6 +24,17 @@ module.exports = function towelSort (matrix) {
 //         [5, 6, 7, 8],
 //         [9, 10, 11, 12],
 //         [13, 14, 15, 16],
-//     ]));
+//     ]));//[ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]
 
-//[ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]
+// console.log(towelSort([[1, 2],
+//     [3, 4]]))//[ 1, 2, 4, 3 ]
+// console.log(towelSort([
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16],
+// ]))//[ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]
+
+// console.log(towelSort()); ///[]
+
+// console.log(towelSort([]))///[]
